fe1050d6bf3f5b4649c1bde0d6d1a4109f7bea48
秉虎 <s96016641@gmail.com>
2021-02-03 15:28:34 +0800

    Revert "[fix] meow"
    This reverts commit a333b3c8

--- app/Http/Controllers/ListController.php
+++ app/Http/Controllers/ListController.php
@@ -2,65 +2,41 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\CashierListDate;
 use App\Http\Requests\CashierRefund;
 use App\Models\CashierList;
 use App\Models\Set;
 use App\Models\TimeRange;
 use Illuminate\Http\Request;
-use Carbon\Carbon;
 
 class ListController extends Controller
 {
-    public function createNewList(CashierListDate $request)
+    public function createNewList(CashierRefund $request)
     {
         $request->validated();
 
-        return $list;
-    }
-
-    public function createNewList(Request $request)
-    {   
-        // $request->validated();
-
-        if (is_null($request->start_date) || is_null($request->end_date)) {
+        if (is_null($request->start_date)) {
             // run default
             $start_date = TimeRange::find(TimeRange::RET)->start_time;
             $end_date = today();
         } else {
-            $start_date = Carbon::createFromFormat('Y-m-d H:i:s', $request->start_date.' 00:00:00', 'Asia/Taipei')->setTimezone('UTC');
-            $end_date = Carbon::createFromFormat('Y-m-d H:i:s', $request->end_date.' 23:59:59', 'Asia/Taipei')->setTimezone('UTC');
+            $start_date = $request->start_date;
+            $end_date = $request->end_date;
         }
 
         $sets = Set::all()
             ->whereNull('list_id')
             ->whereBetween('returned', [$start_date, $end_date]);
 
-        if (count($sets) > 0) {
-            $list = new CashierList();
-            $list->save();
-            foreach ($sets as $set) {
-                $set->forceFill([
-                    'list_id' => $list->id,
-                ])->save();
-            }
-            $list->fresh();
+        $list = new CashierList();
+        $list->save();
 
-            $response = [
-                "ok" => true,
-                "list_id" => $list->id,
-                "sets" => $sets
-            ];
-
-        } else {
-            $response = [
-                "ok" => false,
-                "list_id" => null,
-                "sets" => $sets
-            ];
+        foreach ($sets as $set) {
+            $set->forceFill([
+                'list_id' => $list->id,
+            ])->save();
         }
 
-        return $response;
+        return $list->fresh();
     }
 
     public function refunded(CashierRefund $request)
