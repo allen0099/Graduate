63ce6309664bd7124af97c27c86eac44cc7aaf2a
秉虎 <s96016641@gmail.com>
2021-02-04 00:58:57 +0800

    feat: list update, add status code to list database
    - add status code for list
    - add get not listed sets route
    - add list by
    status code route
    - rewrite create new list method, allow list of set
    ids

--- app/Models/CashierList.php
+++ app/Models/CashierList.php
@@ -9,21 +9,18 @@ class CashierList extends Model
 {
     use HasFactory;
 
+    const CODE_1 = 1; // 已建立批次
+    const CODE_2 = 2; // (退款中) 送出出納組
+    const CODE_3 = 3; // (已退款) 出納組完成
+
+    const CODE_ARRAY = [self::CODE_1, self::CODE_2, self::CODE_3];
+
     /**
      * The table associated with the model.
      *
      * @var string
      */
     protected $table = 'lists';
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'refund' => 'boolean',
-    ];
 
     /**
      * The relations to eager load on every query.
--- database/migrations/2020_11_24_000012_create_lists_table.php
+++ database/migrations/2020_11_24_000012_create_lists_table.php
@@ -15,7 +15,7 @@ class CreateListsTable extends Migration
     {
         Schema::create('lists', function (Blueprint $table) {
             $table->id();
-            $table->boolean('refund')->default(false);
+            $table->integer('status')->default(0);
             $table->timestamps();
         });
     }
