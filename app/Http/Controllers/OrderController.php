c038d1814f7c9021f59f7cd0fc66bfe0dba8d312
秉虎 <s96016641@gmail.com>
2021-02-28 03:52:00 +0800

    fix: store empty string error

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -92,7 +92,9 @@ class OrderController extends Controller
         $order->document_id = $request->document_id;
         $order->owner_id = User::where('username', $request->owner_username)->first()->id;
         $order->status_code = $request->status_code;
-        $order->payment_id = $request->payment_id;
+
+        if ($request->exists('payment_id'))
+            $order->payment_id = $request->payment_id;
 
         if (!is_null($request->sets))
             $order->total_price = $this->calculateTotalPrice($request->sets);
