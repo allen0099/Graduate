2451fc6a21e968dd537124d88d0ed432a900b058
Bandit <rex978956@gmail.com>
2021-03-02 03:37:51 +0800

    [fix] tailwindcss warn when npm run production

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
+    future: {
+        purgeLayersByDefault: true
+    },
     purge: [
         "./vendor/laravel/jetstream/**/*.blade.php",
         "./storage/framework/views/*.php",
