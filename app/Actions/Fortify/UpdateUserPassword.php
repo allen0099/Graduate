9c046b8b5043c3cbaf4140121a3882a9024df641
Bandit <rex978956@gmail.com>
2021-03-11 00:31:01 +0800

    [fix] import log

--- app/Actions/Fortify/UpdateUserPassword.php
+++ app/Actions/Fortify/UpdateUserPassword.php
@@ -5,6 +5,7 @@ namespace App\Actions\Fortify;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Laravel\Fortify\Contracts\UpdatesUserPasswords;
+use Illuminate\Support\Facades\Log;
 
 class UpdateUserPassword implements UpdatesUserPasswords
 {
