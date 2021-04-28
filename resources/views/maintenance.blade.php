3f2cf6519a9d7b88e44d0a57d10a58489127de41
Bandit <rex978956@gmail.com>
2021-04-28 13:22:32 +0800

    [fix] remove mark

--- resources/views/maintenance.blade.php
+++ resources/views/maintenance.blade.php
@@ -9,14 +9,5 @@
             <div class="font-bold text-xl text-yellow-600">很抱歉~ 網站正在維護中！</div>
             <div class="font-bold text-xl text-yellow-600">請於維護後再使用各項功能服務，謝謝！</div>
         </div>
-
-        <div class="mt-8 grid md:grid-rows-1 grid-rows-3 grid-flow-col gap-2">
-            <a href="{{ route('find_pdf', ['name' => '公告1']) }}" target="_blank"
-                class="text-center text-green-600 hover:text-purple-700">{{ '@' }}{{ \App\Models\Config::getPdfName('pdf_a') }}</a>
-            <a href="{{ route('find_pdf', ['name' => '公告2']) }}" target="_blank"
-                class="text-center text-green-600 hover:text-purple-700">{{ '@' }}{{ \App\Models\Config::getPdfName('pdf_b') }}</a>
-            <a href="{{ route('find_pdf', ['name' => '公告3']) }}" target="_blank"
-                class="text-center text-green-600 hover:text-purple-700">{{ '@' }}{{ \App\Models\Config::getPdfName('pdf_c') }}</a>
-        </div>
     </div>
 </x-guest-layout>
